c44c9c98cb2fd96b5e412e7a0e66eb169f461abf
Melton Smith <ravenglc1@gmail.com>
2020-12-06 03:03:42 +0500

    equals and hashcode on Entity

--- src/main/java/domain/abstracts/Entity.java
+++ src/main/java/domain/abstracts/Entity.java
@@ -3,6 +3,7 @@ package domain.abstracts;
 import services.IdGenerator;
 
 import javax.validation.constraints.NotNull;
+import java.util.Objects;
 
 /**
  * @author Melton Smith
@@ -30,5 +31,20 @@ public abstract class Entity {
 
     public String getTitle() {
         return title;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        else if (!(obj instanceof Entity))
+            return false;
+
+        return this.id.equals(((Entity) obj).id);
     }
 }
